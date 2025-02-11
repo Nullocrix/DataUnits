22c2a4f62e1dd7d57836a475841afd2bc898fb48
Miguel Karlo Romero <miguel2070romero@gmail.com>
2025-02-11 12:35:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ setup(
     author="Nullocrix",
     author_email="nullocrix@gmail.com",
     description="A Python module for converting binary units and SI to bytes and vice-versa.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Nullocrix/DataUnits",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["README.md"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
